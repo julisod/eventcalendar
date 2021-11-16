ab014cd8330f4b65e961edf2569d9c7e7b58fef9
Julia Söderlund <julia.soderlund@yandex.ru>
2021-11-17 01:34:59 +0200

    added crossorigin

--- src/main/java/hh/swd20/eventcalendar/web/RestController.java
+++ src/main/java/hh/swd20/eventcalendar/web/RestController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +25,14 @@ public class RestController {
 	// RESTful service to get all events
     // Java-kielinen Event-luokan oliolista muunnetaan JSON-opiskelijalistaksi (en kyl tiiä miten) ja 
     // lähetetään web-selaimelle vastauksena
+	@CrossOrigin
     @RequestMapping(value="/events", method = RequestMethod.GET)
     public @ResponseBody List<Event> eventListRest() {	
         return (List<Event>) erepository.findAll();
     }    
 
 	// RESTful service to get event by id
+	@CrossOrigin
     @RequestMapping(value="/events/{id}", method = RequestMethod.GET)
     public @ResponseBody Optional<Event> findEventRest(@PathVariable("id") Long eventId) {	
     	return erepository.findById(eventId);
